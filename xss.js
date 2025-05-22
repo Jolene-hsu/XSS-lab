9b03ba9289f731c74108a45791b1c3f6902cf926
Jolene-hsu <81403938+Jolene-hsu@users.noreply.github.com>
2025-05-22 17:22:57 +0800

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,7 +1,14 @@
-alert('XSS 成功！')
+// 1. 一般 alert
+alert('XSS 成功！');
+
+// 2. 自我執行
 (function(){
   alert('✅ 自我執行的 XSS！');
 })();
+
+// 3. 混淆呼叫
 (window['al'+'ert'])('✅ 混淆版 XSS！');
+
+// 4. 變數呼叫
 var x = 'al' + 'ert';
 window[x]('✅ 變數混淆 XSS！');
